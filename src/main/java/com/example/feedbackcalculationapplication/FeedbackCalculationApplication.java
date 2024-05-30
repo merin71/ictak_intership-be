7d8842bd941f6f45340cd41ba0dbfb4812bff777
Merin <merint2000@gmail.com>
2024-05-30 20:54:06 +0530

    initial changes

---
+++ src/main/java/com/example/feedbackcalculationapplication/FeedbackCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.example.feedbackcalculationapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackCalculationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/feedbackcalculationapplication/FeedbackCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feedbackcalculationapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackCalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
